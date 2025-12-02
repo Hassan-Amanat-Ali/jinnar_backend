d34921b49ad2f08a81f25a88e875d0b5a7e90f0d
Abdullah Nauman <Abdullahsf99@gmail.com>
2025-12-02 09:26:53 +0500

    updated firebase account to jinnarTraining account

--- src/config/firebase_config.js
+++ src/config/firebase_config.js
@@ -1,17 +1,17 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyC71hBg73rpkySVVZ86B3B7pPXYiT7O8TE",
-  authDomain: "tanzaniaservices-739c2.firebaseapp.com",
-  projectId: "tanzaniaservices-739c2",
-  storageBucket: "tanzaniaservices-739c2.firebasestorage.app",
-  messagingSenderId: "1007107385150",
-  appId: "1:1007107385150:web:2ead6cd5f212abd105ee6f",
-};
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC71hBg73rpkySVVZ86B3B7pPXYiT7O8TE",
+//   authDomain: "tanzaniaservices-739c2.firebaseapp.com",
+//   projectId: "tanzaniaservices-739c2",
+//   storageBucket: "tanzaniaservices-739c2.firebasestorage.app",
+//   messagingSenderId: "1007107385150",
+//   appId: "1:1007107385150:web:2ead6cd5f212abd105ee6f",
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
