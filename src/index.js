84f6c7c01e6d13f9668f74abd8c839fd6940d546
Abdullah Nauman <Abdullahsf99@gmail.com>
2026-03-15 00:11:18 +0500

    fixed cors error

--- src/index.js
+++ src/index.js
@@ -74,6 +74,7 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:3001', 
   'http://localhost:5173',
+  'https://www.jinnar.com',
   'https://jinnar.com',
   'https://training.jinnar.com',
   'https://viral.jinnar.com'
